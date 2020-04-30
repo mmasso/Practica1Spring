11112af32e5002cda740b4b1c4488200da85fbfb
mmasso <mmasso@cifpfbmoll.eu>
2020-04-30 11:39:00 +0200

    feat(ServeiAlumnat): Test3 matricula

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -5,15 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class ServeiAlumnat {
 
-	
+	@Autowired //Afegim notació Spring
+	private RepositoriAlumnes repositori;
+
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
 	 * el corresponent codi.
 	 * Si el nom de l'alumne es null, no l'ha de donar d'alta
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
-	public boolean matricula (int id, String alumne) {
-		return true;
+	public boolean matricula(int id, String alumne) {
+		if (alumne != null) {
+			repositori.altaAlumne(id, alumne);
+			return true;}
+		return false;
 	}
-	
 }
